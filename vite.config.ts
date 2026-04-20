8809e606c2fe7cc52b3802005745b7fb1c746880
DKGJJANG <dkgjjang@naver.com>
2026-04-20 11:03:50 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,12 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    build: {
+      target: 'es2022'
+    },
+    esbuild: {
+      target: 'es2022'
+    },
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
